862053c24d7389e7c5c47058e033ffff0003017f
Siddhartha Sadhukhan <siddhartha.sadhukhan.2014@gmail.com>
2019-05-19 16:33:27 +0530

    Intellij Commit

--- src/main/scala/com/sid/learn/example/ScalaCustomException.scala
+++ src/main/scala/com/sid/learn/example/ScalaCustomException.scala
@@ -6,6 +6,7 @@ class InvalidAgeException(s:String) extends Exception(s){}
 class ExceptionExample{
   @throws(classOf[InvalidAgeException])
   def validate(age:Int){
+
     if(age<18){
       throw new InvalidAgeException("Not eligible")
     }else{
